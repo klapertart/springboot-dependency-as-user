d9aa5bc36ce56b3e76f85dbeb7fd48131dcb6bba
Otong Sunandar <otong.sunandar@tritronik.com>
2023-03-29 23:05:15 +0700

    add (test): Crud operation from dependency project

---
+++ src/test/java/com/example/demo1/MemberServiceTest.java
@@ -0,0 +1,32 @@
+package com.example.demo1;
+
+import com.example.demo2.entities.Member;
+import com.example.demo2.services.MemberService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author kurakuraninja
+ * @since 29/03/23
+ */
+@SpringBootTest
+public class MemberServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Test
+    @Disabled
+    public void testSave(){
+        Member member = Member.builder()
+                .name("Hanna")
+                .build();
+
+        final Member save = memberService.save(member);
+
+        Assertions.assertEquals(member.getName(),save.getName());
+    }
+}
